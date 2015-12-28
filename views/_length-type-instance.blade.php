2346250bce4e8a620a37de7f8f8cc8b894fae85f
Oliver Green <green2go@gmail.com>
2015-12-28 19:05:55 +0000

    Smartened up instance form.

--- views/_length-type-instance.blade.php
+++ views/_length-type-instance.blade.php
@@ -1,25 +1,30 @@
-<label>
-{{ $control->getFormLabel() }}
-</label>
-
 <div class="form-group {{ ($control->hasFormErrors()) ? 'has-error' : '' }}">
 
-    <input name="{{ $control->getFormName('length') }}" type="text" value="{{ $control->getFormValue('length') }}" class="form-control" />
+    <label for="{{ $control->getFormName() }}">
+        {{ $control->getFormLabel() }}
+    </label>
 
-    <select name="{{ $control->getFormName('units') }}" class="form-control">
-        <option value="">Please select...</option>
-        <option value="mm" {{ 'mm' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>mm</option>
-        <option value="cm" {{ 'cm' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>cm</option>
-        <option value="m" {{ 'm' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>m</option>
-        <option value="km" {{ 'km' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>km</option>
-    </select>
-    @if ($control->hasFormErrors())
-        <span id="helpBlock" class="help-block">
-            <ul>
+    <div class="row">
+        <div class="col-xs-8">
+            <input name="{{ $control->getFormName('length') }}" type="text" value="{{ $control->getFormValue('length') }}" class="form-control" />
+        </div>
+        <div class="col-xs-4" style="padding-left: 0;">
+            <select name="{{ $control->getFormName('units') }}" class="form-control">
+                <option value="">Please select...</option>
+                <option value="mm" {{ 'mm' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>mm</option>
+                <option value="cm" {{ 'cm' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>cm</option>
+                <option value="m" {{ 'm' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>m</option>
+                <option value="km" {{ 'km' === $control->getFormValue('units') ? 'selected="selected"' : '' }}>km</option>
+            </select>
+        </div>
+        @if ($control->hasFormErrors())
+        <div class="col-xs-12">
+            <span id="helpBlock" class="help-block">
                 @foreach (array_flatten($control->getFormErrors()) as $error)
-                <li>{{ $error }}</li>
+                <p>{{ $error }}</p>
                 @endforeach
-            </ul>
-        </span>
-    @endif
+            </span>
+        </div>
+        @endif
+    </div>
 </div>
